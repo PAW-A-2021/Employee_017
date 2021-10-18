00ef2276968c8035f5a892c260473a0deb00cd45
yahyaluqman <yahya.luqmanu21@gmail.com>
2021-10-18 21:34:56 +0700

    Add project files.

---
+++ Employee_017/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employee_017.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
